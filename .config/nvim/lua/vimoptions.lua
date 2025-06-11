4ce8a337c79cd9a8b4bb799d5f743a7ec970f561
TheNextMyth <56999833+TheNextMyth@users.noreply.github.com>
2025-06-11 17:19:46 -0500

    Add vimoptions

---
+++ .config/nvim/lua/vimoptions.lua
@@ -0,0 +1,14 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+local builtin = require('telescope.builtin')
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fa", function()
+                        builtin.find_files({ hidden = true })
+                end, { desc = "Find files including gitignored and hidden" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
